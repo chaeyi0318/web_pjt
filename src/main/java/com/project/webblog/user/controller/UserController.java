2d6cf6b8eb796cb70b591d47cebba737c90feee8
Chaeyi <chaeyi0318@gmail.com>
2024-01-28 21:25:23 +0900

    feat : 회원가입 구현

--- src/main/java/com/project/webblog/user/controller/UserController.java
+++ src/main/java/com/project/webblog/user/controller/UserController.java
@@ -9,9 +9,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,9 +18,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
     private final UserService userService;
 
+    @GetMapping("/signup")
+    public ModelAndView mainPage() {
+        return new ModelAndView("signup");
+    }
+
     @PostMapping("/signup")
     @Operation(summary = "회원가입")
-    public ResponseEntity<ApiResponse> signup(@RequestBody SignupRequestDto requestDto) {
+    public ResponseEntity<ApiResponse> signup(SignupRequestDto requestDto) {
         return userService.signup(requestDto);
     }
 }
