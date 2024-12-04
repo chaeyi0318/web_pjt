9dd250dc11a85fccbf204f5bddd313d5b9d434e1
chaeyi0318 <chaeyi0318@gmail.com>
2024-12-04 19:08:56 +0900

    Init: 프로젝트 세ã세팅

---
+++ partyParty/src/main/java/com/project/partyparty/PartyPartyApplication.java
@@ -0,0 +1,13 @@
+package com.project.partyparty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartyPartyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartyPartyApplication.class, args);
+	}
+
+}
